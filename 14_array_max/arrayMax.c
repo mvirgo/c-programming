d27ed2639ae74ec2231e1d8235b44f188e913ce9
Coursera Learner <mvirgo@github.com>
2018-12-30 19:10:23 +0000

    Max of C array assignment

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if (n==0) {
+    return NULL;
+  }
+  int max = 0;
+  for (int i=1; i<n; ++i) {
+    if (array[i] > array[max]) {
+      max = i;
+    }
+  }
+  return array+max;
 }
 
 void doTest(int * array, int n) {
