63f9d77f84d149a320213f21e9cd04ff079ece99
Coursera Learner <mvirgo@github.com>
2019-01-02 05:00:59 +0000

    Add maxSeq implementation

---
+++ 16_subseq/maxSeq.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t maxSeq(int * array, size_t n) {
+  if (n == 0) {
+    return NULL;
+  }
+  int max = 1;
+  int current = 1;
+  for (int i=1; i < n; ++i) {
+    if (array[i] > array[i-1]) {
+      current += 1;
+    } else {
+      current = 1;
+    }
+    if (current > max) {
+      max = current;
+    }
+  }
+
+  return max;
+}
